9b97be9b6279f1d2df6773d29effd3bb16918497
vinay kumar <vinay.kumar@domingo.in>
2019-09-20 18:01:11 +0530

    some change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { retry, catchError, map } from 'rxjs/operators'
 
+import { first } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo';
+  dataValue:any=[]
+  headElements=['Title','Url','Created_at','Author']
+  apiUrl="https://hn.algolia.com/api/v1"
+  constructor( private http:HttpClient){
+    
+   this.getData();
+  }
+
+  ngOnInit(){
+   
+  }
+
+  getUsers() {
+    debugger
+    return this.http.get<any>(this.apiUrl + `/search_by_date?tags=story`)
+      .pipe(
+        // 
+        retry(1),
+        
+      )
+  }
+
+  getData(){
+    this.getUsers()
+    .pipe(first())
+      .subscribe(
+        data => {
+          debugger
+          this.dataValue=data.hits
+         
+        },
+        error => {
+         
+        }
+      )
+  }
+
+  getUserDetails(id){
+    this.getUsers()
+    .pipe(first())
+      .subscribe(
+        data => {
+          debugger
+          for (let i=0; i<=data.hits.length;i++){
+            if(id == i){
+              this.dataValue=data.hits
+            }
+          }
+          // this.dataValue=data.hits
+         
+        },
+        error => {
+         
+        }
+      )
+  }
 }
